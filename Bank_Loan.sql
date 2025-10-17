57cf9ea067bd6f7b1eedb1df55abfb7cccec4328
Paisalkhan <152694678+Paisal-Khan@users.noreply.github.com>
2025-10-17 17:29:39 +0530

    Add files via upload

---
+++ Bank_Loan.sql
@@ -0,0 +1,226 @@
+use LoanDB
+-- Total Loan Applications
+SELECT COUNT(id) AS Total_Applications FROM Loan_Data
+
+-- MTD Loan Applications
+SELECT COUNT(id) AS Total_Applications 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12;
+
+--PMTD Loan Applications
+SELECT COUNT(id) AS Total_Applications 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 11;
+
+--Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount 
+FROM Loan_Data
+
+-- MTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12;
+
+--PMTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 11;
+
+--Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected 
+FROM Loan_Data
+
+--MTD Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12;
+
+--PMTD Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 11
+
+--Average Interest Rate
+SELECT AVG(int_rate) * 100 AS Avg_Int_Rate 
+FROM Loan_Data
+
+--MTD Average Interest
+SELECT AVG(int_rate) * 100 AS MTD_Avg_Int_Rate 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12
+
+--PMTD Average Interest
+SELECT AVG(int_rate) * 100 AS PMTD_Avg_Int_Rate 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 11
+
+--Avg DTI
+SELECT AVG(dti) * 100 AS Avg_DTI 
+FROM Loan_Data
+
+--MTD Avg DTI
+SELECT AVG(dti) * 100 AS MTD_Avg_DTI 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12
+
+--PMTD Avg DTI
+SELECT AVG(dti) * 100 AS PMTD_Avg_DTI 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 11
+
+--Good Loan Percentage
+SELECT
+    (COUNT(CASE WHEN loan_status = 'Fully Paid' OR loan_status = 'Current' THEN id END) * 100.0) / COUNT(id) AS Good_Loan_Percentage
+FROM Loan_Data
+
+--Good Loan Applications
+SELECT COUNT(id) AS Good_Loan_Applications 
+FROM Loan_Data
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
+
+--Good Loan Funded Amount
+SELECT SUM(loan_amount) AS Good_Loan_Funded_Amount 
+FROM Loan_Data
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
+
+--Good Loan Amount Received
+SELECT SUM(total_payment) AS Good_Loan_Amount_Received 
+FROM Loan_Data
+WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
+
+--Bad Loan Percentage
+SELECT
+    (COUNT(CASE WHEN loan_status = 'Charged Off' THEN id END) * 100.0) / COUNT(id) AS Bad_Loan_Percentage
+FROM Loan_Data
+
+--Bad Loan Applications
+SELECT COUNT(id) AS Bad_Loan_Applications 
+FROM Loan_Data
+WHERE loan_status = 'Charged Off'
+
+--Bad Loan Funded Amount
+SELECT SUM(loan_amount) AS Bad_Loan_Funded_Amount 
+FROM Loan_Data
+WHERE loan_status = 'Charged Off'
+
+--Bad Loan Amount Received
+SELECT SUM(total_payment) AS Bad_Loan_Amount_Received 
+FROM Loan_Data
+WHERE loan_status = 'Charged Off'
+
+--Loan Status Overview
+SELECT
+    loan_status,
+    COUNT(id) AS LoanCount,
+    SUM(total_payment) AS Total_Amount_Received,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    AVG(int_rate * 100) AS Interest_Rate,
+    AVG(dti * 100) AS DTI
+FROM Loan_Data
+GROUP BY loan_status
+
+--MTD Loan Status Summary
+SELECT 
+    loan_status, 
+    SUM(total_payment) AS MTD_Total_Amount_Received, 
+    SUM(loan_amount) AS MTD_Total_Funded_Amount 
+FROM Loan_Data
+WHERE MONTH(issue_date) = 12 
+GROUP BY loan_status
+
+--Monthly Overview
+SELECT 
+    MONTH(issue_date) AS Month_Number, 
+    DATENAME(MONTH, issue_date) AS Month_Name, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
+ORDER BY MONTH(issue_date)
+
+--State Overview
+SELECT 
+    address_state AS State, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY address_state
+ORDER BY address_state
+
+--Term Overview
+SELECT 
+    term AS Term, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY term
+ORDER BY term
+
+--Employee Length Overview
+SELECT 
+    emp_length AS Employee_Length, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY emp_length
+ORDER BY emp_length
+
+--Purpose Overview
+SELECT 
+    purpose AS Purpose, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY purpose
+ORDER BY purpose
+
+--Home Ownership Overview
+SELECT 
+    home_ownership AS Home_Ownership, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+GROUP BY home_ownership
+ORDER BY home_ownership
+
+--Filter Example (Grade = 'A')
+SELECT 
+    purpose AS Purpose, 
+    COUNT(id) AS Total_Loan_Applications,
+    SUM(loan_amount) AS Total_Funded_Amount,
+    SUM(total_payment) AS Total_Amount_Received
+FROM Loan_Data
+WHERE grade = 'A'
+GROUP BY purpose
+ORDER BY purpose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
